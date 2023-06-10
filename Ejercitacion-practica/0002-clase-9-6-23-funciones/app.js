f1429c1f41fb1881d149765cb496dbb67e671146
Adriana Villafañe <tsade@live.com.ar>
2023-06-10 20:47:19 -0300

    primer funcion arrow

---
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/app.js
@@ -0,0 +1,8 @@
+let num1= (prompt(' Ingrese un numero'));
+let num2= (prompt('Ingrese otro numero'));
+
+const suma =(num1,num2)=>{
+    console.log(Number(num1)+Number(num2));
+};
+
+suma(num1,num2);
