a5ffb0e21d5eacaafc6f0e02e72261eadec52140
Adriana Villafañe <tsade@live.com.ar>
2023-06-24 09:04:22 -0300

    hasta ejercicio 29

--- Ejercitacion-practica/0002-clase-9-6-23-funciones/28app.js
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/28app.js
@@ -3,7 +3,13 @@
 
 //FPS son _cuadros por segundo_ (_frames per second_). Crear una una función `calcularFPS` que tome como argumentos una cantidad de cuadros por segundo y una cantidad de minutos, y devuelva cuántos cuadros hubo en esa cantidad de minutos
 
+let cuadrosPorSeg;
+let minutos;
 
-//calcularFPS(1, 1) // 60
-calcularFPS(10, 2) // 1200
-calcularFPS(2, 3) // 360
+ const calcularFPS=(cuadrosPorSeg, minutos)=>{
+ return (cuadrosPorSeg * minutos*60);
+
+ }
+console.log(calcularFPS(1, 1)); // 60
+console.log(calcularFPS(10, 2)); // 1200
+console.log(calcularFPS(2, 3));// 360
--- Ejercitacion-practica/0002-clase-9-6-23-funciones/29app.js
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/29app.js
@@ -1,9 +1,13 @@
 //ejercicio 29
 // `obtenerCompetencia(a, b)`
 
-//Crear una función `obtenerCompetencia` que tome como argumentos dos strings `a` y `b` y devuelva un string con el formato `a vs. b`
+//Crear una función `obtenerRivales` que tome como argumentos dos strings `a` y `b` y devuelva un string con el formato `a vs. b`
+
+const obtenerRivales=(a,b)=>{
+    return`${a} Vs ${b}`;
+}
 
 //javascript
-obtenerRivales('JavaScript', 'Python') // `JavaScript vs. Python`
-obtenerRivales('Coca', 'Pepsi') // `Coca vs. Pepsi`
-obtenerRivales('Perros', 'Gatos') // `Perros vs. Gatos`
+console.log(obtenerRivales('JavaScript', 'Python')); // `JavaScript vs. Python`
+console.log(obtenerRivales('Coca', 'Pepsi')); // `Coca vs. Pepsi`
+console.log(obtenerRivales('Perros', 'Gatos')); // `Perros vs. Gatos`
