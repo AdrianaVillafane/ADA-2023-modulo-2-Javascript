c649c3dfb89290e272961f1095e60ae6d39db03f
Adriana Villafañe <tsade@live.com.ar>
2023-06-24 09:59:43 -0300

    ejercicio 32

--- Ejercitacion-practica/0002-clase-9-6-23-funciones/32app.js
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/32app.js
@@ -1,10 +1,17 @@
 //ejercicio 32
-//### `haceCalor(temperatura)`
+// `haceCalor(temperatura)`
 
 //Crear una función `haceCalor` que tome como argumento un número `temperatura` y devuelva `true` si hace calor (22 grados o más) o `false` de lo contrario
 
-```javascript
-haceCalor(12) // false
-haceCalor(22) // true
-haceCalor(32) // true
-```
+
+const haceCalor=(temperatura)=>{
+    if(temperatura>=22){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+console.log(haceCalor(12)); // false
+console.log(haceCalor(22)); // true
+console.log(haceCalor(32)); // true
