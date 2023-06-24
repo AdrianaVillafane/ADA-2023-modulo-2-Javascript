9646bf12aafa89fa7f294a8be4c9e4469f635c90
Adriana Villafañe <tsade@live.com.ar>
2023-06-24 12:20:53 -0300

    ejercicio 35

--- Ejercitacion-practica/0002-clase-9-6-23-funciones/35app.js
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/35app.js
@@ -1,10 +1,19 @@
-//### `aceptaDeposito(monto)`
+//35
+//`aceptaDeposito(monto)`
 
 //Crear una función `aceptaDeposito` que tome como argumento un número `monto` y devuelva `true` si el `monto` es divisible por 10 o `false` si no lo es
 
-```javascript
-aceptaDeposito(440) // true
-aceptaDeposito(123) // false
-aceptaDeposito(500.50) // false
-aceptaDeposito(1000) // true
-```
+
+const aceptaDeposito=(monto)=>{
+
+    if( monto % 10 == 0){
+        return true;
+    } else{
+        return false;
+    }
+}
+
+console.log(aceptaDeposito(440)); // true
+console.log(aceptaDeposito(123)); // false
+console.log(aceptaDeposito(500.50)); // false
+console.log(aceptaDeposito(1000)); // true
