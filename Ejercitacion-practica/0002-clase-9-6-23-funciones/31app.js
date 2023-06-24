76ca3730a0425ae4e554bf3c72d3afe618c37432
Adriana Villafañe <tsade@live.com.ar>
2023-06-24 09:46:25 -0300

    ejercicio 31

--- Ejercitacion-practica/0002-clase-9-6-23-funciones/31app.js
+++ Ejercitacion-practica/0002-clase-9-6-23-funciones/31app.js
@@ -2,9 +2,16 @@
 // `esMayorDeEdad(edad)`
 
 //Crear una función `esMayorDeEdad` que tome como argumento un número `edad` y devuelva `true` si es mayor de edad (18 o más) o `false` de lo contrario
+const esMayorDeEdad=(edad)=>{
+    if(edad>= 18){
+        return true;
+    }else{
+        return false;
+    }
+}
 
-```javascript
-esMayorDeEdad(15) // false
-esMayorDeEdad(18) // true
-esMayorDeEdad(27) // true
-```
+
+
+console.log(esMayorDeEdad(15)); // false
+console.log(esMayorDeEdad(18)); // true
+console.log(esMayorDeEdad(27)); // true
